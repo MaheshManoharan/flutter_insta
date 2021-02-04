e024e3491625181bca56b41cd7bef10d7fa30b6c
mahesh manoharan <maheshmanoharan7251@gmail.com>
2021-02-04 09:47:42 +0530

    Post uploaded successfully

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,17 +1,73 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_insta/models/user.dart';
+import 'package:flutter_insta/pages/home.dart';
 import 'package:flutter_insta/widgets/header.dart';
+import 'package:flutter_insta/widgets/progress.dart';
 
 class Profile extends StatefulWidget {
+  final String profileId;
+
+  Profile({this.profileId});
+
   @override
   _ProfileState createState() => _ProfileState();
 }
 
 class _ProfileState extends State<Profile> {
+  buildProfileHeader() {
+    return FutureBuilder(
+      future: usersRef.document(widget.profileId).get(),
+      builder: (context, snapshot) {
+        if (!snapshot.hasData) {
+          return circularProgress();
+        }
+        User user = User.fromDocument(snapshot.data);
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  CircleAvatar(
+                    radius: 40.0,
+                    backgroundColor: Colors.grey,
+                    backgroundImage: CachedNetworkImageProvider(user.photoUrl),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            // buildCountColumn(),
+                            // buildCountColumn(),
+                            // buildCountColumn(),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: header(context, titleText: "Profile"),
-      body: Text("Profile"),
+      body: ListView(
+        children: [
+          buildProfileHeader(),
+        ],
+      ),
     );
   }
 }
